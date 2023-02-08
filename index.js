353a1691c70da70231b8fe2336fcf721e82fa1c4
JoeDaIV <Joedaiv60@gmail.com>
2023-02-08 02:00:00 +0000

    add getDistance method

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-(function(window, _) {
-    window.myLibraryName = window.myLibraryName || {
-      numz: {},
-      phyz: {},
-    };
-  }(window, window._));
+(function (window, _) {
+  window.mugiwara = window.mugiwara || {
+    numz: {},
+    phyz: {
+      /**
+       * calculate the distance
+       * @param {object} pointA - the first point on screen
+       * @param {number} pointA.y - the y coordinate for pointA
+       * @param {number} pointA.x - the x coordinate for pointA
+       * @param {*} pointB - the second point on screen
+       * @param {number} pointB.y - the y coordinate for pointB
+       * @param {number} pointB.x - the x coordinate for pointB
+       * @returns {number} returns the distance
+       */
+      getDistance(pointA, pointB) {
+        const
+
+          distanceX = pointB.x - pointA.x,
+
+          distanceY = pointB.y - pointA.y,
+
+          distance = Math.sort(distanceX * distanceX + distanceY * distanceY);
+
+        return distance;
+
+
+      }
+    },
+  };
+}(window, window._));
+
+
+mugiwara.phyz.calcDistance()
